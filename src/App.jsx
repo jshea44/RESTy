7228f5ac6b75a47919a94d391a2fa100c6f40aa3
joshuashea <joshshea44@gmail.com>
2023-10-23 13:39:43 -0700

    refactored App.jsx to use function instead of class

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,80 @@
 import React from 'react';
+import { useState } from 'react';
 
 import './App.scss';
 
 // Let's talk about using index.js and some other name in the component folder.
 // There's pros and cons for each way of doing this...
-// OFFICIALLY, we have chosen to use the Airbnb style guide naming convention. 
+// OFFICIALLY, we have chosen to use the Airbnb style guide naming convention.
 // Why is this source of truth beneficial when spread across a global organization?
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 
-class App extends React.Component {
+function App(props) {
+  const [dataObj, setData] = useState({});
+  const [requestParamsObj, setRequestParams] = useState({});
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
-
-  callApi = (requestParams) => {
+  function callApi(dataObj, requestParamsObj) {
     // mock output
     const data = {
       count: 2,
       results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
       ],
     };
-    this.setState({data, requestParams});
+    // this.setState({data, requestParams});
+    setData({ ...dataObj, data });
+    setRequestParams({ ...requestParamsObj, requestParamsObj });
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Request Method: {requestParamsObj.method}</div>
+      <div>URL: {requestParamsObj.url}</div>
+      <Form handleApiCall={callApi} />
+      <Results data={dataObj} />
+      <Footer />
+    </React.Fragment>
+  );
 }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: null,
+//       requestParams: {},
+//     };
+//   }
+
+//   callApi = (requestParams) => {
+//     // mock output
+//     const data = {
+//       count: 2,
+//       results: [
+//         { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+//         { name: 'fake thing 2', url: 'http://fakethings.com/2' },
+//       ],
+//     };
+//     this.setState({ data, requestParams });
+//   };
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Header />
+//         <div>Request Method: {this.state.requestParams.method}</div>
+//         <div>URL: {this.state.requestParams.url}</div>
+//         <Form handleApiCall={this.callApi} />
+//         <Results data={this.state.data} />
+//         <Footer />
+//       </React.Fragment>
+//     );
+//   }
+// }
 
 export default App;
