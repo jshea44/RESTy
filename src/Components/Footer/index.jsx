1b563094583995c480bf8f8e908eca5bd38db161
joshuashea <joshshea44@gmail.com>
2023-10-23 13:54:09 -0700

    refactored Footer to use a function

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+// import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+function Footer() {
+  return <footer>&copy; 2018</footer>;
 }
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
 
 export default Footer;
