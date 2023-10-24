fb983057855593b2fbd6b1b46c7410846d752f5f
joshuashea <joshshea44@gmail.com>
2023-10-24 15:47:37 -0700

    finished almost all of lab except test

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,26 +1,17 @@
-// import React from 'react';
 import './Results.scss';
 
 function Results(props) {
   return (
     <section>
-      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      {props.loading ? (
+        <p>Loading...</p>
+      ) : (
+        <pre>
+          {props.data ? JSON.stringify(props.data, undefined, 2) : null}
+        </pre>
+      )}
     </section>
   );
 }
-
-// class Results extends React.Component {
-//   render() {
-//     return (
-//       <section>
-//         <pre>
-//           {this.props.data
-//             ? JSON.stringify(this.props.data, undefined, 2)
-//             : null}
-//         </pre>
-//       </section>
-//     );
-//   }
-// }
 
 export default Results;
